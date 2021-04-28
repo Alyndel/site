974cbea99b388b936fdf7fd1b8a874af6d6a067d
Hugo <hugo.nitard@gmail.com>
2021-04-28 16:26:23 +0200

    jpp de bootstrap

--- header.php
+++ header.php
@@ -1,19 +1,19 @@
     <header>
         <nav class="navbar navbar-dark bg-dark">
-            <div class="container-fluid">
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
                             <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#" aria-disabled="true">CV</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#" aria-disabled="true">Réalisations</a>
-                        </li>
-                    </ul>
+                            <a class="nav-link" href="#" aria-disabled="true">CV</a>
+                            <a class="nav-link" href="#" aria-disabled="true">Réalisations</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </nav>
         </nav>
     </header>
